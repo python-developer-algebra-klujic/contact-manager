5108d45a31e76f3bbfcf10067dfd507131f4ac29
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-03-17 19:11:21 +0100

    Finished Contacts and Customers

--- features/customers/models/customers.py
+++ features/customers/models/customers.py
@@ -1,5 +1,18 @@
+from typing import List
+
+from features.contacts.models.contacts import Contact
 from features.shared.models.companies import Company
 
 
 class Customer(Company):
-    pass
+    def __init__(self,
+                 id: int,
+                 name: str,
+                 vat_id: str,
+                 headquarter: str,
+                 contacts: List[Contact] = []):
+        super().__init__(id, name, vat_id, headquarter)
+        self.contacts = contacts
+
+    def __repr__(self):
+        return f'{self.name} ({self.vat_id}) / ({len(self.contacts)})'
